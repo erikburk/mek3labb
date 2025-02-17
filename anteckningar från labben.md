43787070925d91c520ebe34fa80dabca599612b1
erikburk <erik.bjork.13@gmail.com>
2025-02-17 14:00:57 +0100

    plottning

---
+++ anteckningar från labben.md
@@ -0,0 +1,47 @@
+# Mek3 la1
+
+## Mätningar
+vikt på hela stången: 106.51 g +- 0.005g
+massan: 70.45 g
+stång utan massa: 36.05 g
+
+diameter på stången: 0.85 cm +- ganska mycket bror vi vet inte hur man mäter
+längd på stången med massa: 56cm +- 0.5cm
+längd mellan kanten och O: 2.51 cm
+Längd mellan O och P: 14.5cm
+höjd på massa: 2.07 cm
+innerdiameter på massan: 0.85 cm
+ytter: 2.50 cm
+
+### Mätning av k:
+delta x = 18.5 cm +- 0.5cm
+m = 50g +- 0.5g
+k = 2.654 N/m
+
+## Runs och sånt
+Fall A: 4st runs
+använder run 3
+
+Fall B: 4st
+använder run 2
+
+Fall C: 4st
+använder run 3
+
+## Beats och beräkningar
+Från Lab C, run 3
+O_1 = o- - o+ / 2 = 0.265 rad/s
+O_2 = o+ + o- / 2 = 7.07 rad/s
+k bör bli typ 3
+
+### I
+Runt MC: 
+I_stång = 1/12 ml^2 (runt mc) = 7.249 E-7 kg-m^2
+I_massa = 1.4796 E-5 kg-m^2
+
+Runt O:
+Io = 0.02398 kgm^2
+
+Teoretiska omega:
+o1 = 5.685 rad/s
+o2 = 6.080 rad/s
--- plot.py
+++ plot.py
@@ -3,20 +3,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+labellist = ('$ \phi $(t)',r'$ \theta $(t)','$q_1$(t)','$q_2$(t)')
+
 df = pd.read_csv('FallA.csv',delimiter=';')
 
-df.info()
-
 viktig_data = df.iloc[:1000,10:15]
+
+plt.figure()
+for i in range(1,5):
+    plt.plot(viktig_data.iloc[:,0],
+        viktig_data.iloc[:,i]-viktig_data.iloc[:,i].mean(),
+        label=labellist[i-1])
+plt.xlabel('Tid (s)')
+plt.ylabel('Vinkel (rad)')
+plt.title('Fall A')
+plt.legend()
+plt.savefig('plot_A.pdf',bbox_inches='tight')
+plt.show()
+
+
+df = pd.read_csv('FallB.csv',delimiter=';')
+
+viktig_data = df.iloc[:1000,6:11]
 viktig_data.info()
 
 
 plt.figure()
-plt.plot(viktig_data.iloc[:,0],viktig_data.iloc[:,1],label='$ \phi $(t)')
-plt.plot(viktig_data.iloc[:,0],viktig_data.iloc[:,2],label='$ \theta $(t)')
-plt.plot(viktig_data.iloc[:,0],viktig_data.iloc[:,3],label='$q_1$(t)')
-plt.plot(viktig_data.iloc[:,0],viktig_data.iloc[:,4],label='$q_2$(t)')
-plt.xlabel('Time (s)')
-plt.ylabel('Angle (rad)')
+for i in range(1,5):
+    plt.plot(viktig_data.iloc[:,0],
+        viktig_data.iloc[:,i]-viktig_data.iloc[:,i].mean(),
+        label=labellist[i-1])
+plt.xlabel('Tid (s)')
+plt.ylabel('Vinkel (rad)')
+plt.title('Fall B')
 plt.legend()
+plt.savefig('plot_B.pdf',bbox_inches='tight')
+plt.show()
+
+df = pd.read_csv('FallC.csv',delimiter=';')
+
+viktig_data = df.iloc[:1000,10:15]
+
+plt.figure()
+for i in range(1,5):
+    plt.plot(viktig_data.iloc[:,0],
+        viktig_data.iloc[:,i]-viktig_data.iloc[:,i].mean(),
+        label=labellist[i-1])
+plt.xlabel('Tid (s)')
+plt.ylabel('Vinkel (rad)')
+plt.title('Fall C')
+plt.legend()
+plt.savefig('plot_C.pdf',bbox_inches='tight')
+
 plt.show()
